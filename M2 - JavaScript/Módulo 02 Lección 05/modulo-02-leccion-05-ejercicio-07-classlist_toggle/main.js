d31a528de2619d3b40706c6fe696e9bdb15cce91
Alba V <albavidalm@gmail.com>
2021-05-06 23:27:51 +0200

    Add exercice 06

---
+++ M2 - JavaScript/Módulo 02 Lección 05/modulo-02-leccion-05-ejercicio-07-classlist_toggle/main.js
@@ -0,0 +1,13 @@
+"use strict";
+
+const input = document.querySelector(".my-input");
+const wrote = document.querySelector(".paragraph");
+
+function handleTakeInfo(event) {
+  //console.log(input);
+  const letter = event.currentTarget.value;
+  //console.log(letter);
+  wrote.innerHTML = letter;
+}
+
+input.addEventListener("keypress", handleTakeInfo);
