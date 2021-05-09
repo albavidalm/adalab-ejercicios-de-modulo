b2bc19f1ee9a09084f74e65513ef510142242343
Alba V <albavidalm@gmail.com>
2021-05-09 13:41:06 +0200

    Add ex 07

---
+++ M2 - JavaScript/Módulo 02 Lección 06/modulo-02-leccion-06-ejercicio-07/main.js
@@ -0,0 +1,16 @@
+"use strict";
+
+const userInfo = {};
+
+const job = "developer";
+
+userInfo.firstName = "Alba";
+userInfo.lastName = "Vidal";
+userInfo.age = 41;
+userInfo.job = job;
+//console.log(userInfo.job);
+console.log(userInfo);
+
+userInfo.firstName = "Elvira";
+userInfo.age = 13;
+console.log(userInfo);
