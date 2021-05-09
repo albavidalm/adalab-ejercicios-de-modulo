55926f041d86785ed2374bba7bf6ca31c62ffa1a
Alba V <albavidalm@gmail.com>
2021-05-09 12:12:51 +0200

    Add exercice 05

---
+++ M2 - JavaScript/Módulo 02 Lección 06/modulo-02-leccion-06-ejercicio-05-elementos-HTML/main.js
@@ -0,0 +1,12 @@
+"use strict";
+
+const input = document.querySelector(".js-input");
+console.dir(input);
+
+/*  PROPIEDADES:
+· value: No le hemos dado ningún valos, sigue vacío.
+
+· nodeName: "name" El nombre de la propiedad de la etiqueta name
+
+· required: false Si hubiéramos marcado este campo como requerido sería true
+*/
