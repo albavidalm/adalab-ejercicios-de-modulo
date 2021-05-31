5afed2365bca1fb8495d54222d952a5863962ce0
Alba V <albavidalm@gmail.com>
2021-05-31 21:55:53 +0200

    Add M3

---
+++ M3 - React/Lección 01 - mediacard/src/components/App.js
@@ -0,0 +1,44 @@
+import "../stylesheets/App.css";
+import image from "../images/C34.jpg";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="Cardcontainer">
+        <div class="CardHeader">
+          <img
+            className="Profilepicture"
+            src={image}
+            title="Skull"
+            alt="Skull and candle"
+          ></img>
+          <div class="Cardheader2">
+            <h2 className="Username">All Bones</h2>
+            <h3 className="Date">Lunes, 31 de mayo de 2021</h3>
+          </div>
+        </div>
+        <p className="Infouser">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed viverra,
+          ipsum vitae consectetur pulvinar, mi lacus tincidunt nisi, non maximus
+          mi nunc ut leo. Mauris ut ligula accumsan, pulvinar urna in, venenatis
+          lorem. Integer auctor, nibh ut semper porttitor, nisi lectus lobortis
+          nibh, eu tincidunt eros risus eu massa. Phasellus lacinia magna vel
+          arcu sodales, at consectetur metus facilisis. Donec imperdiet faucibus
+          justo, et rhoncus dolor fermentum venenatis. Ut ornare, tortor sit
+          amet laoreet lobortis, mi diam egestas quam, at pharetra massa massa
+          eu dolor.
+        </p>
+
+        <a className="Moreinfo" href="#">
+          Leer más...
+        </a>
+        <div class="Postinfo">
+          <p className="Likes">37</p>
+          <p className="Heart">♥</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
