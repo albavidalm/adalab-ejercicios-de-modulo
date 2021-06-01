6d33137b1c31c8907f4ade4f7735e8ab1fa86376
Alba V <albavidalm@gmail.com>
2021-06-01 20:49:41 +0200

    Add ex 01

---
+++ M3 - React/Lección 02/Ejercicio 01 - mediacard componentes/src/components/App.js
@@ -0,0 +1,15 @@
+/* eslint-disable no-useless-constructor */
+import React from "react";
+import MediaCard from "./MediaCard";
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <MediaCard />
+      </div>
+    );
+  }
+}
+
+export default App;
