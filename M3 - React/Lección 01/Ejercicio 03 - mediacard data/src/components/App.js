7a83477a1711fae0de120fa41b5b30cd87b012a1
Alba V <albavidalm@gmail.com>
2021-06-01 18:38:28 +0200

    Add ex 03

--- M3 - React/Lección 01/Ejercicio 03 - mediacard data/src/components/App.js
+++ M3 - React/Lección 01/Ejercicio 03 - mediacard data/src/components/App.js
@@ -22,37 +22,25 @@ class App extends React.Component {
       },
     };
 
-    //const main = <div className="Cardcontainer"></div>;
     const header = (
       <div class="CardHeader">
         <img
           className="Profilepicture"
-          src={image}
+          src={data.image}
           title="Skull"
           alt="Skull and candle"
         ></img>
         <div class="Cardheader2">
           <h2 className="Username">{data.cardTitle}</h2>
-          <h3 className="Date">Lunes, 31 de mayo de 2021</h3>
+          <h3 className="Date">{data.cardDate}</h3>
         </div>
       </div>
     );
-    const text = (
-      <p className="Infouser">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed viverra,
-        ipsum vitae consectetur pulvinar, mi lacus tincidunt nisi, non maximus
-        mi nunc ut leo. Mauris ut ligula accumsan, pulvinar urna in, venenatis
-        lorem. Integer auctor, nibh ut semper porttitor, nisi lectus lobortis
-        nibh, eu tincidunt eros risus eu massa. Phasellus lacinia magna vel arcu
-        sodales, at consectetur metus facilisis. Donec imperdiet faucibus justo,
-        et rhoncus dolor fermentum venenatis. Ut ornare, tortor sit amet laoreet
-        lobortis, mi diam egestas quam, at pharetra massa massa eu dolor.
-      </p>
-    );
+    const text = <p className="Infouser">{data.cardDescription}</p>;
     const footer = (
       <div>
-        <a className="Moreinfo" href="#">
-          Leer más...
+        <a href={data.button.url} target="_blank">
+          <input type="button" value={data.button.label} className="button" />
         </a>
         <div class="Postinfo">
           <p className="Likes">37</p>
