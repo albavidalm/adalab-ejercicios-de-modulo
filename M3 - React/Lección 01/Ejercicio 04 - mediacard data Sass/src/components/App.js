842658617fdc8ffc30f808d411341e882894ab85
Alba V <albavidalm@gmail.com>
2021-06-01 18:53:12 +0200

    Add ex 04

---
+++ M3 - React/Lección 01/Ejercicio 04 - mediacard data Sass/src/components/App.js
@@ -0,0 +1,61 @@
+/* eslint-disable no-useless-constructor */
+import "./App.scss";
+import React from "react";
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    const data = {
+      image:
+        "https://ucarecdn.com/f8cf81eb-3bab-4bba-9431-668884eab174/-/resize/300x/",
+      cardTitle: "Bob Dylan",
+      cardDate: "May 24, 1941",
+      cardDescription:
+        "Bob Dylan (born Robert Allen Zimmerman, May 24, 1941) is an American singer/songwriter, author, and artist who has been an influential figure in popular music and culture for more than five decades.",
+      button: {
+        url: "https://en.wikipedia.org/wiki/Bob_Dylan",
+        label: "Go to wikipedia",
+      },
+    };
+
+    const header = (
+      <div class="CardHeader">
+        <img
+          className="Profilepicture"
+          src={data.image}
+          title="Skull"
+          alt="Skull and candle"
+        ></img>
+        <div class="Cardheader2">
+          <h2 className="Username">{data.cardTitle}</h2>
+          <h3 className="Date">{data.cardDate}</h3>
+        </div>
+      </div>
+    );
+    const text = <p className="Infouser">{data.cardDescription}</p>;
+    const footer = (
+      <div>
+        <a href={data.button.url} target="_blank">
+          <input type="button" value={data.button.label} className="button" />
+        </a>
+        <div class="Postinfo">
+          <p className="Likes">37</p>
+          <p className="Heart">♥</p>
+        </div>
+      </div>
+    );
+
+    return (
+      <div className="App">
+        <div className="Cardcontainer">
+          {header} {text} {footer}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
